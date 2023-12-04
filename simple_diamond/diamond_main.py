07630ebc5622a7dee5dc533bd7b9c92153781cbf
khe9370 <kevin.q.he@gmail.com>
2023-12-04 18:42:18 -0500

    Changes

--- simple_diamond/diamond_main.py
+++ simple_diamond/diamond_main.py
@@ -7,6 +7,9 @@ import double_indexing
 parser = argparse.ArgumentParser()
 parser.add_argument('--query', type=str, required=True)
 parser.add_argument('--protein_database', type=str, required=True)
+parser.add_argument('--double_indexing_type', type=str, required=True)
+parser.add_argument('--sketching', type=str, required=True)
+parser.add_argument('--double_indexing_type', type=str, required=True)
 parser.add_argument('--save_path',  type=str, required=True)
 args = parser.parse_args()
 
@@ -19,6 +22,9 @@ with open(args.protein_database, encoding="utf-8") as myFile:
 
 query_dictionary, query_dictionary_reduced = read_fasta.read_fasta_for_DNA(query_sequences)
 protein_database_dictionary, protein_database_dictionary_reduced = read_fasta.read_fasta_for_proteins(protein_database_sequences)
+
+if args.double_indexing_type == "double_indexing_type": 
+    output_file = double_indexing
 
 sorted_query_reduced, sorted_protein_database_reduced = double_indexing.double_indexing_iterator(query_dictionary, protein_database_dictionary, query_dictionary_reduced, protein_database_dictionary_reduced, "111101101101")
 
