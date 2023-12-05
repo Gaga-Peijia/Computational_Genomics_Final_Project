0352bda1037be5ff566421ce7347de838205f007
khe9370 <kevin.q.he@gmail.com>
2023-12-05 10:42:48 -0500

    Smith Waterman

--- simple_diamond/full_smithwaterman.py
+++ simple_diamond/full_smithwaterman.py
@@ -14,7 +14,7 @@ def SMalignment(seq1, seq2, penalty_matrix):
     m = len(seq1)
     n = len(seq2)
     
-    g = -3
+    g = -5
     matrix = []
     for i in range(0, m):
         tmp = []
@@ -30,48 +30,68 @@ def SMalignment(seq1, seq2, penalty_matrix):
             matrix[siii][sjjj] = max(matrix[siii-1][sjjj] + g, matrix[siii - 1][sjjj - 1] + SingleBaseCompare(seq1,seq2,siii, sjjj,penalty_matrix), matrix[siii][sjjj-1] + g)
     sequ1 = [seq1[m-1]]
     sequ2 = [seq2[n-1]]
-    align_line = []
     while m > 1 and n > 1:
         if max(matrix[m-1][n-2], matrix[m-2][n-2], matrix[m-2][n-1]) == matrix[m-2][n-2]:
             m -= 1
             n -= 1
             sequ1.append(seq1[m-1])
             sequ2.append(seq2[n-1])
-            if seq1[m-1]==seq2[n-1]:
-                align_line.append('|')
-            else:
-                align_line.append(' ')
         elif max(matrix[m-1][n-2], matrix[m-2][n-2], matrix[m-2][n-1]) == matrix[m-1][n-2]:
             n -= 1
-            align_line.append(' ')
             sequ1.append('-')
             sequ2.append(seq2[n-1])
         else:
             m -= 1
             sequ1.append(seq1[m-1])
             sequ2.append('-')
-            align_line.append(' ')
     sequ1.reverse()
     sequ2.reverse()
-    align_line.reverse()
     align_seq1 = ''.join(sequ1)
     align_seq2 = ''.join(sequ2)
-    align_lining = ''.join(align_line)
     align_score = 0.
-    for k in range(0, len(align_seq1)):
-        if align_seq1[k] == align_seq2[k]:
-            align_score += 1
-    align_score = float(align_score)/len(align_seq1)
-    return align_lining, align_seq1, align_seq2, align_score
-"""
+    gapcount_seq1 = 0
+    gapcount_seq2 = 0
+    '''
+    for k in range(0, len(sequ1)):
+        scoring = SingleBaseCompare(sequ1,sequ2,k,k, penalty_matrix)
+        align_score += scoring
+    '''
+    aligning = []
+    for k in range(0, len(sequ1)):
+        if sequ1[k]!='-' and sequ2[k]!='-':
+            if sequ1[k]== sequ2[k]:
+                aligning.append('|')
+            else:
+                aligning.append(' ')
+            scoring = SingleBaseCompare(sequ1,sequ2,k,k, penalty_matrix)
+            align_score += scoring
+            if k-1>=0 and sequ1[k-1]=='-':
+                align_score-=11+gapcount_seq1
+                gapcount_seq1 = 0
+            elif k-1>=0 and sequ2[k-1]=='-':
+                align_score-=11+gapcount_seq2
+                gapcount_seq2 = 0
+        elif sequ1[k]=='-':
+            aligning.append(' ')
+            gapcount_seq1+=1
+        elif sequ2[k]=='-':
+            aligning.append(' ')
+            gapcount_seq2+=1
+        
+    aligning_sequence = ''.join(aligning)
+    
+
+    align_score = float(align_score)
+    
+    return aligning_sequence, align_seq1, align_seq2, align_score
+
 # Get the BLOSUM62 matrix
 BLOSUM62 = bl.BLOSUM(62)
 # Example usage
-seq1 = "DEAAADEAAA"
-seq2 = "DWAADEAAA"
+seq1 = "DFNVILMSTCGAGKEDAG"
+seq2 = "DFEVIISTCGAAKRNKDAG"
 aligning, align_1, align_2, alignment_score = SMalignment(seq1, seq2, BLOSUM62)
 print(align_1)
 print(aligning)
 print(align_2)
 print("Alignment Score:", alignment_score)
-"""
