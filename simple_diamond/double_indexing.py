6ba8074a6d4c5cf807aad0abc8effbbef8e8cacd
khe9370 <kevin.q.he@gmail.com>
2023-12-07 10:06:09 -0500

    Double Indexing

--- simple_diamond/double_indexing.py
+++ simple_diamond/double_indexing.py
@@ -4,12 +4,59 @@ import sketching
 import sort_merge_join
 #import smith_waterman
 import datetime
+import blosum as bl
+import full_smithwaterman
+
+BLOSUM62 = bl.BLOSUM(62)
+
+def best_match(dict, querydatabase, protein_database,seed_length):
+    with open("alignment_results.txt", "w") as file:
+        #initialize the local variable
+        query_seq = ""
+        query_len = 0
+        best_reading_frame_score = 0
+        best_reading_frame = 0
+        starting_position_of_reference = 0
+        edge = 0
+        reading_frame_list = []
+        for item in dict:
+            #item is the name of the query
+            for protein_name in dict[item]:
+                protein_seq = protein_database[protein_name]
+                for reading_frame in dict[item][protein_name]:
+                    #reset the variable every time we traverse each reading frame
+                    query_seq = querydatabase[item][reading_frame]
+                    query_len = len(query_seq)
+                    best_reading_frame_score = 0
+                    best_reading_frame = 0
+                    starting_position_of_reference = 0
+                    edge = 0
+                    #reginal smithwarterman
+                    #need to convert set to list and sort it 
+                    reading_frame_list = list(dict[item][protein_name][reading_frame])
+                    reading_frame_list.sort()
+                    for index in reading_frame_list:
+                        if index>=edge:  # it should be >= since edge can be 0
+                            start = max(0, index - query_len)
+                            end = min(query_len, index + seed_length + query_len)
+                            edge = end  #extending the edge
+                            sequence_snippet = protein_seq[start:end]
+                            aligning_sequence, query_align, protein_align, score = full_smithwaterman.SMalignment(query_seq, sequence_snippet, BLOSUM62)
+                            if score>best_reading_frame_score:
+                                best_reading_frame_score = score
+                                starting_position_of_reference = start
+                                if best_reading_frame!=reading_frame:
+                                    best_reading_frame = reading_frame
+                #after each iteration
+                file.write(f"{item}\t{protein_name}\t{best_reading_frame_score}\t{starting_position_of_reference}\t{best_reading_frame}\n")    
+                    #return aligning_sequence, align_seq1, align_seq2, align_score
+                    
+
 
 
 
 def double_indexing_iterator(query_database, protein_database, reduced_query_database, reduced_protein_database, shapes, sketching_method):
     query_database_seed_hits = {}    
-    
     for query in reduced_query_database:
         query_database_seed_hits[query] = {}
         
@@ -51,6 +98,8 @@ def double_indexing_iterator(query_database, protein_database, reduced_query_dat
                 time = last_time - first_time
                 print("1 iter complete in " + str(time))
     print(query_database_seed_hits)
+    best_match(query_database_seed_hits, query_database, protein_database, len(max(shapes, key=len)))
+    
     """        
                 #merged_dict = sort_merge_join.sort_merge_join_dicts(query_dictionary, , shape)
 
